b34a350795853d52807032d63e5787688af69a6a
eupita <doniojv7@gmail.com>
2026-09-14 21:29:13 -0300

    Add files via upload

---
+++ Site Valo/scripts.js
@@ -0,0 +1,24 @@
+let prevButton = document.getElementById("prev");
+let nextButton = document.getElementById("next");
+let container = document.querySelector(".Container");
+let items = container.querySelectorAll(".list .item");
+
+let active = 0;
+let firstPosition = 0;
+let lastPosition = items.length - 1;
+
+nextButton.onclick = () => {
+    let itemOld = container.querySelector(".list .item.active");
+    itemOld.classList.remove("active");
+
+    active = active + 1 > lastPosition ? 0 : active + 1
+    items[active].classList.add("active");
+}
+
+prevButton.onclick = () => {
+    let itemOld = container.querySelector(".list .item.active");
+    itemOld.classList.remove("active");
+
+    active = active - 1 < firstPosition ? lastPosition : active - 1;
+    items[active].classList.add("active");
+}
